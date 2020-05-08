e7e4d400d53a2d76ae07d6409e2f5c8385ce13cc
grzes <grzesek98@gmail.com>
2020-05-08 14:35:58 +0200

    Added function for plotting word2vec in 2D

--- glove.py
+++ glove.py
@@ -1,12 +1,54 @@
 from gensim.scripts.glove2word2vec import glove2word2vec
 from gensim.models.keyedvectors import KeyedVectors
+from gensim import models
+import numpy as np
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import pandas as pd
+from stop_words import get_stop_words
 
-#glove2word2vec(glove_input_file="D:\\projects\\inf_retrieval\\datasets\\glove\\glove.6B.300d.txt",
-#               word2vec_output_file="D:\\projects\\inf_retrieval\\datasets\\glove\\gensim_glove_vectors.txt")
+def convert_to_word2vec():
+    glove2word2vec(glove_input_file="D:\\projects\\inf_retrieval\\datasets\\glove\\glove.6B.300d.txt",
+              word2vec_output_file="D:\\projects\\inf_retrieval\\datasets\\glove\\gensim_glove_vectors.txt")
 
-glove_model = KeyedVectors.load_word2vec_format("D:\\projects\\inf_retrieval\\datasets\\glove\\gensim_glove_vectors.txt", binary=False, limit=78000)
-#print(glove_model["beautiful"])
-print(glove_model.most_similar("girl"))
 
-hist_levy_model = KeyedVectors.load_word2vec_format("D:\\projects\\inf_retrieval\\datasets\\levy\\HistoLevy.txt", binary=False, limit=78000)
-print(hist_levy_model.most_similar("girl"))
+def load_word2vec(file_path):
+    glove_model = KeyedVectors.load_word2vec_format(
+        file_path, binary=False, limit=1000)
+
+    return glove_model
+
+
+def compare_simmilar(w, model):
+    print(model.most_similar(w))
+
+
+def plot_wor2vec(model):
+    vocab = list(model.wv.vocab)
+    X = model[vocab]
+    tsne = TSNE(n_components=2)
+    X_tsne = tsne.fit_transform(X)
+    positions = pd.DataFrame(X_tsne, columns=['x', 'y'])
+    words = pd.DataFrame(vocab, columns=['word'])
+    df = pd.concat([words, positions], axis=1)
+    df['word'] = df['word'].astype(str)
+    print(df)
+
+    # remove stopwords from the dataframe
+    stop_words = list(get_stop_words('en'))
+    df = df[~df["word"].isin(stop_words)]
+    print(df)
+
+    # plot
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.scatter(df['x'], df['y'])
+    for q in df.index:
+        ax.annotate(df['word'][q], (df['x'][q], df['y'][q]))
+    plt.show()
+
+if __name__ == "__main__":
+    glove_model = load_word2vec("D:\\projects\\inf_retrieval\\datasets\\glove\\gensim_glove_vectors.txt")
+    plot_wor2vec(glove_model)
+    #compare_simmilar("girl", glove_model)
+
