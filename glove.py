8f331f1a9a2d376a3a6797290faee33e2d517994
grzes <grzesek98@gmail.com>
2020-05-08 19:26:13 +0200

    Added function for converting word2vec to pandas dataframe

--- glove.py
+++ glove.py
@@ -14,17 +14,38 @@ def convert_to_word2vec():
 
 def load_word2vec(file_path):
     glove_model = KeyedVectors.load_word2vec_format(
-        file_path, binary=False, limit=1000)
+        file_path, binary=False, limit=78000)
 
     return glove_model
 
 
 def compare_simmilar(w, model):
+    """as an input provide word2vec"""
     print(model.most_similar(w))
 
 
+def convert_to_pandas_remove_stopwords(model):
+    """as an input provide word2vec"""
+    vocab = list(model.vocab)
+    stop_words = list(get_stop_words('en'))
+    punctuation = ['</s>', '@', ',', '.', "''", ';', '--', "'", '!', "n't", '?', '#', '"', '(', ')', '[', ']', '~', '-', "'s"]
+    to_remove = []
+    for q in vocab:
+        if q in stop_words or q in punctuation:
+            to_remove.append(q)
+    for e in to_remove:
+        vocab.remove(e)
+    pos = model[vocab]
+    positions = pd.DataFrame(pos)
+    words = pd.DataFrame(vocab, columns=['word'])
+    df = pd.concat([words, positions], axis=1)
+    return df
+
+
 def plot_wor2vec(model):
+    """as an input provide pandas word2vec"""
     vocab = list(model.wv.vocab)
+    print(vocab)
     X = model[vocab]
     tsne = TSNE(n_components=2)
     X_tsne = tsne.fit_transform(X)
@@ -33,12 +54,6 @@ def plot_wor2vec(model):
     df = pd.concat([words, positions], axis=1)
     df['word'] = df['word'].astype(str)
     print(df)
-
-    # remove stopwords from the dataframe
-    stop_words = list(get_stop_words('en'))
-    df = df[~df["word"].isin(stop_words)]
-    print(df)
-
     # plot
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
@@ -47,8 +62,19 @@ def plot_wor2vec(model):
         ax.annotate(df['word'][q], (df['x'][q], df['y'][q]))
     plt.show()
 
+
+def compare_models(model1, model2):
+    """as an input provide two pandas dataframes"""
+
+
 if __name__ == "__main__":
-    glove_model = load_word2vec("D:\\projects\\inf_retrieval\\datasets\\glove\\gensim_glove_vectors.txt")
-    plot_wor2vec(glove_model)
+    #glove_model = load_word2vec("D:\\projects\\inf_retrieval\\datasets\\glove\\gensim_glove_vectors.txt")
+    #plot_wor2vec(glove_model)
+
+    levy_model = load_word2vec("D:\\projects\\inf_retrieval\\datasets\\levy\\HistoLevy.txt")
+    print(levy_model)
+    levy_no_stopwords = convert_to_pandas_remove_stopwords(levy_model)
+    print(levy_no_stopwords)
+    #plot_wor2vec(levy_model)
     #compare_simmilar("girl", glove_model)
 
