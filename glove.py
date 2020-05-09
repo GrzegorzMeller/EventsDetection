c47c069422cb2b7d2cac750e3ae807ea1fb78d8e
grzes <grzesek98@gmail.com>
2020-05-09 11:12:44 +0200

    Added function that finds intersection between two models

--- glove.py
+++ glove.py
@@ -12,9 +12,9 @@ def convert_to_word2vec():
               word2vec_output_file="D:\\projects\\inf_retrieval\\datasets\\glove\\gensim_glove_vectors.txt")
 
 
-def load_word2vec(file_path):
+def load_word2vec(file_path ,lim):
     glove_model = KeyedVectors.load_word2vec_format(
-        file_path, binary=False, limit=78000)
+        file_path, binary=False, limit=lim)
 
     return glove_model
 
@@ -28,7 +28,7 @@ def convert_to_pandas_remove_stopwords(model):
     """as an input provide word2vec"""
     vocab = list(model.vocab)
     stop_words = list(get_stop_words('en'))
-    punctuation = ['</s>', '@', ',', '.', "''", ';', '--', "'", '!', "n't", '?', '#', '"', '(', ')', '[', ']', '~', '-', "'s"]
+    punctuation = ['</s>', '@', ',', '.', "''", ';', '--', "'", '!', "n't", '?', '#', '"', '(', ')', '[', ']', '~', '-', "'s", "'m", "'re", '/']
     to_remove = []
     for q in vocab:
         if q in stop_words or q in punctuation:
@@ -65,16 +65,21 @@ def plot_wor2vec(model):
 
 def compare_models(model1, model2):
     """as an input provide two pandas dataframes"""
+    df = pd.DataFrame(columns=["word", "cos_distance"])
+    for q in model1.index:
+        if model1["word"][q] in model2["word"].values:
+            print(model1["word"][q])
 
 
 if __name__ == "__main__":
-    #glove_model = load_word2vec("D:\\projects\\inf_retrieval\\datasets\\glove\\gensim_glove_vectors.txt")
-    #plot_wor2vec(glove_model)
+    glove_model = load_word2vec("D:\\projects\\inf_retrieval\\datasets\\glove\\gensim_glove_vectors.txt", 200000) #lim=200000
+    glove_no_stopwords = convert_to_pandas_remove_stopwords(glove_model)
 
-    levy_model = load_word2vec("D:\\projects\\inf_retrieval\\datasets\\levy\\HistoLevy.txt")
-    print(levy_model)
+    levy_model = load_word2vec("D:\\projects\\inf_retrieval\\datasets\\levy\\HistoLevy.txt", 78041) #lim=78041
     levy_no_stopwords = convert_to_pandas_remove_stopwords(levy_model)
-    print(levy_no_stopwords)
+
+    compare_models(levy_no_stopwords, glove_no_stopwords)
+
     #plot_wor2vec(levy_model)
     #compare_simmilar("girl", glove_model)
 
