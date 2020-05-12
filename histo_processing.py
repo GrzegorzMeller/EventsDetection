0b0af607f6a5a6464a815de2fe65762e1c1a5125
grzes <grzesek98@gmail.com>
2020-05-12 19:04:40 +0200

    Added function for reading txt file written in BIO format and creating list of tuples

--- histo_processing.py
+++ histo_processing.py
@@ -2,7 +2,9 @@ import xml.dom.minidom
 
 #to do: sliding window on text, and annotation on each window if it contatins event or not
 
+
 def get_text():
+    '''input file should be in xml format'''
     doc = xml.dom.minidom.parse("D:\\projects\\inf_retrieval\\datasets\\news\\Anarchists_Demand_Strike_To_End_War.txt.xml")
     tokens = doc.getElementsByTagName("token")
     iterator = 0
@@ -15,5 +17,25 @@ def get_text():
     print(text)
 
 
+def create_tuple_list():
+    '''input file should be in bio format'''
+    tuple_list = []
+    sentence = []
+    doc = open(
+        "D:\\projects\\inf_retrieval\\datasets\\bio_mention\\train.txt","r")
+    for line in doc:
+        if line.strip():
+            words = line.split()
+            t = (words[0], words[1], words[2], words[3], words[4], words[5])
+            sentence.append(t)
+        else:
+            tuple_list.append(sentence)
+            sentence = []
+    return tuple_list
+
+
 if __name__ == "__main__":
-    get_text()
+    #get_text()]
+
+    tuple_list = create_tuple_list()
+    print(tuple_list[0])
