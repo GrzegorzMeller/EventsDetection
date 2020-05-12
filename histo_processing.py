83bfd51496770b70d785d228119d9e18809f1c70
grzes <grzesek98@gmail.com>
2020-05-12 19:21:06 +0200

    Added function for extracting features from words

--- histo_processing.py
+++ histo_processing.py
@@ -1,21 +1,6 @@
-import xml.dom.minidom
-
-#to do: sliding window on text, and annotation on each window if it contatins event or not
-
-
-def get_text():
-    '''input file should be in xml format'''
-    doc = xml.dom.minidom.parse("D:\\projects\\inf_retrieval\\datasets\\news\\Anarchists_Demand_Strike_To_End_War.txt.xml")
-    tokens = doc.getElementsByTagName("token")
-    iterator = 0
-    text = ""
-    for t in tokens:
-        token = doc.getElementsByTagName("token")[iterator]
-        element = token.childNodes[0]
-        text += element.nodeValue+" "
-        iterator += 1
-    print(text)
-
+import sklearn_crfsuite
+from sklearn_crfsuite import scorers
+from sklearn_crfsuite import metrics
 
 def create_tuple_list():
     '''input file should be in bio format'''
@@ -26,7 +11,7 @@ def create_tuple_list():
     for line in doc:
         if line.strip():
             words = line.split()
-            t = (words[0], words[1], words[2], words[3], words[4], words[5])
+            t = (words[0], words[2], words[5])
             sentence.append(t)
         else:
             tuple_list.append(sentence)
@@ -34,8 +19,65 @@ def create_tuple_list():
     return tuple_list
 
 
+def word2features(sent, i):
+    word = sent[i][0]
+    postag = sent[i][1]
+
+    features = {
+        'bias': 1.0,
+        'word.lower()': word.lower(),
+        'word[-3:]': word[-3:],
+        'word[-2:]': word[-2:],
+        'word.isupper()': word.isupper(),
+        'word.istitle()': word.istitle(),
+        'word.isdigit()': word.isdigit(),
+        'postag': postag,
+        'postag[:2]': postag[:2],
+    }
+    if i > 0:
+        word1 = sent[i - 1][0]
+        postag1 = sent[i - 1][1]
+        features.update({
+            '-1:word.lower()': word1.lower(),
+            '-1:word.istitle()': word1.istitle(),
+            '-1:word.isupper()': word1.isupper(),
+            '-1:postag': postag1,
+            '-1:postag[:2]': postag1[:2],
+        })
+    else:
+        features['BOS'] = True
+
+    if i < len(sent) - 1:
+        word1 = sent[i + 1][0]
+        postag1 = sent[i + 1][1]
+        features.update({
+            '+1:word.lower()': word1.lower(),
+            '+1:word.istitle()': word1.istitle(),
+            '+1:word.isupper()': word1.isupper(),
+            '+1:postag': postag1,
+            '+1:postag[:2]': postag1[:2],
+        })
+    else:
+        features['EOS'] = True
+
+    return features
+
+
+def sent2features(sent):
+    return [word2features(sent, i) for i in range(len(sent))]
+
+
+def sent2labels(sent):
+    return [label for token, postag, label in sent]
+
+
+def sent2tokens(sent):
+    return [token for token, postag, label in sent]
+
+
 if __name__ == "__main__":
     #get_text()]
 
     tuple_list = create_tuple_list()
-    print(tuple_list[0])
+    print(tuple_list[1])
+    print(sent2features(tuple_list[1])[1])
